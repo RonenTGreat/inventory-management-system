5154f050db69ec20b7eda1665957d8420fcd101c
RonenTGreat <ronenh53@gmail.com>
2023-07-08 23:40:25 +0000

    finished with frontend aspect of design

--- src/main/java/com/hammond320/inventorysystemmanagement/HelloController.java
+++ src/main/java/com/hammond320/inventorysystemmanagement/HelloController.java
@@ -1,7 +1,31 @@
 package com.hammond320.inventorysystemmanagement;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
+
+    @FXML
+    private Button close;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    public void  close(){
+        System.exit(0);
+    }
+
 }
