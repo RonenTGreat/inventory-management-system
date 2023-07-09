8efb1bf1cc8af35e0ff6dd947fb178626b41784c
RonenTGreat <ronenh53@gmail.com>
2023-07-09 01:35:36 +0000

    Created database connection

--- src/main/java/com/hammond320/inventorysystemmanagement/HelloController.java
+++ src/main/java/com/hammond320/inventorysystemmanagement/HelloController.java
@@ -1,11 +1,16 @@
 package com.hammond320.inventorysystemmanagement;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class HelloController {
 
@@ -23,6 +28,57 @@ public class HelloController {
 
     @FXML
     private TextField username;
+
+    private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
+
+    public void loginAdmin(){
+        String sql = "SELECT * FROM admin WHERE username = ? and password = ?";
+
+        connect = database.connectionDb();
+
+        try{
+            prepare = connect.prepareStatement(sql);
+            prepare.setString(1, username.getText());
+            prepare.setString(2, password.getText());
+
+            result = prepare.executeQuery();
+            Alert alert;
+
+
+            if(username.getText().isEmpty() || password.getText().isEmpty()){
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all blank fields");
+                alert.showAndWait();
+            }else{
+                if(result.next()){
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Login!");
+                    alert.showAndWait();
+
+                    Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+
+                    stage.setScene(scene);
+                    stage.show();
+
+                }else{
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Wrong Username or Password");
+                    alert.showAndWait();
+                }
+            }
+        }catch (Exception e){e.printStackTrace();}
+    }
+
 
     public void  close(){
         System.exit(0);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.hammond320.inventorysystemmanagement {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+
+
     opens com.hammond320.inventorysystemmanagement to javafx.fxml;
     exports com.hammond320.inventorysystemmanagement;
 }
