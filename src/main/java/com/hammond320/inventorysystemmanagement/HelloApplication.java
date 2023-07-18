458d691f60290fed2607174666585eed5d9629e7
RonenTGreat <ronenh53@gmail.com>
2023-07-18 13:51:17 +0000

    added name and ID to main class

--- src/main/java/com/hammond320/inventorysystemmanagement/HelloApplication.java
+++ src/main/java/com/hammond320/inventorysystemmanagement/HelloApplication.java
@@ -7,6 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
+/*
+* Ronen Nii Ayi Hammond
+* 10899320
+* */
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
